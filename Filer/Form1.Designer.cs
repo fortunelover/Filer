be73c2e1f7489b4932ea271d036fd543d1fedf19
fortunelover <2671204505@qq.com>
2023-03-13 17:38:23 +0800

    添加项目文件。

---
+++ Filer/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Filer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            FPath = new TextBox();
+            label1 = new Label();
+            File2Folder = new Button();
+            OutPut = new Label();
+            label3 = new Label();
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            SuspendLayout();
+            // 
+            // FPath
+            // 
+            FPath.Location = new Point(62, 51);
+            FPath.Name = "FPath";
+            FPath.Size = new Size(687, 23);
+            FPath.TabIndex = 0;
+            FPath.TextChanged += textBox1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(23, 54);
+            label1.Name = "label1";
+            label1.Size = new Size(33, 17);
+            label1.TabIndex = 1;
+            label1.Text = "Path";
+            // 
+            // File2Folder
+            // 
+            File2Folder.Location = new Point(23, 107);
+            File2Folder.Name = "File2Folder";
+            File2Folder.Size = new Size(102, 27);
+            File2Folder.TabIndex = 2;
+            File2Folder.Text = "文件->文件夹";
+            File2Folder.UseVisualStyleBackColor = true;
+            File2Folder.Click += button1_Click;
+            // 
+            // OutPut
+            // 
+            OutPut.AutoSize = true;
+            OutPut.Location = new Point(80, 195);
+            OutPut.Name = "OutPut";
+            OutPut.Size = new Size(0, 17);
+            OutPut.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft YaHei UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(4, 193);
+            label3.Name = "label3";
+            label3.Size = new Size(69, 25);
+            label3.TabIndex = 4;
+            label3.Text = "输出：";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(OutPut);
+            Controls.Add(File2Folder);
+            Controls.Add(label1);
+            Controls.Add(FPath);
+            Name = "Form1";
+            Text = "Filer";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button File2Folder;
+        private Label OutPut;
+        private TextBox FPath;
+
+
+        private Label label1;
+
+        private Label label3;
+        private FolderBrowserDialog folderBrowserDialog1;
+    }
+}
---
+++ Filer/Form1.cs
@@ -0,0 +1,43 @@
+namespace Filer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var path = this.FPath.Text;
+                DirectoryInfo root = new DirectoryInfo(path);
+                FileInfo[] files = root.GetFiles();
+                foreach (FileInfo file in files)
+                {
+                    var HouZhui = Path.GetExtension(file.FullName);
+                    var newWenJianJia = file.FullName.Replace(HouZhui, "");
+                    if (!Directory.Exists(newWenJianJia))
+                    {
+                        Directory.CreateDirectory(newWenJianJia);
+                    }
+                    file.MoveTo($@"{newWenJianJia}\{file.Name}");
+                }
+                this.OutPut.Text = "³É¹¦";
+
+            }
+            catch (Exception ex)
+            {
+                this.OutPut.Text = "Ê§°Ü\n" + ex;
+            }
+
+
+        }
+    }
+}
